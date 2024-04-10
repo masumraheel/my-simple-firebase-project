0f7b364c4519a580d966fbfe36b07843e58f621b
masumraheel <masumhossain017252@gmail.com>
2024-04-10 13:19:29 +0600

    my first section complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './Components/Home/Home'
+
+function App() {
+  
+
+  return (
+    <>
+      <Home></Home>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
